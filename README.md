b7e6d606be0fc06c614ebfcfd819aa759a72c88f
samiesmilz <samiesmilz@gmail.com>
2023-11-01 20:12:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# Flask Story Generator
+
+A simple Flask web application that generates fun and unique stories based on user inputs. Users can choose from various story templates and fill in the details to create entertaining stories.
+
+## Table of Contents
+
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [Usage](#usage)
+- [Templates](#templates)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Choose from different story templates.
+- Fill in story attributes such as place, noun, verb, adjective, and plural noun.
+- Generate and view unique stories based on your inputs.
+- Fun and entertaining for users of all ages.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- Flask (you can install it using `pip install flask`)
+- Stories module (make sure it's available in your project)
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/samiesmilz/flask-madlibs.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd flask-madlibs
+   ```
+
+3. Set up a virtual environment (optional but recommended):
+
+   ```bash
+   python -m venv venv
+   source venv/bin/activate  # On Windows, use 'venv\Scripts\activate'
+   ```
+
+4. Install required dependencies:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+1. Start the Flask application:
+
+   ```bash
+   python app.py
+   ```
+
+2. Open a web browser and go to [http://localhost:5000](http://localhost:5000) to access the web application.
+
+3. Choose a story template from the provided options.
+
+4. Fill in the story attributes in the form.
+
+5. Click the "Generate Story" button to create your unique story.
+
+6. Enjoy your generated story on the result page.
+
+## Templates
+
+The application includes the following story templates:
+
+- **Default**: A classic story template with placeholders for place, noun, verb, adjective, and plural noun.
+- **Farmyard**: A fun and wacky farmyard-themed story.
+- **Pirate**: An adventurous pirate-themed story.
+- **Space**: A space exploration-themed story.
+- **Superhero**: A story about a zany superhero.
+- **Disaster**: An epic adventure involving time-travel and dinosaurs.
+
+Feel free to customize or add your own templates as needed.
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions or would like to add new features, please submit a pull request. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+- Enjoy creating and sharing entertaining stories with Flask Story Generator!
+- If you have any questions or need further assistance, please don't hesitate to reach out.
+
+### A samie smilz production.
+
+#### Happy coding! 😄
---
+++ app.py
@@ -0,0 +1,86 @@
+from stories import Story
+from flask import Flask, render_template, request
+app = Flask(__name__, static_folder='static')
+
+
+@app.route("/")
+def home():
+    """ Render the form on the homepage """
+    return render_template('index.html')
+
+
+# Variable to store the template selected
+template = None
+
+
+@app.route("/form")
+def show_form():
+    """ Render the form to collect story attributes """
+
+    # Get selected template
+    global template
+    template = request.args["story_template"]
+
+    return render_template('form.html')
+
+
+@app.route("/story")
+def show_story():
+    """ A method to costruct the story and render it on the page """
+
+    # Extarct all the story words from the form once submitted
+    place = request.args.get("place", "no-where")
+    noun = request.args.get("noun", "na-la")
+    verb = request.args.get("verb", "_ _ _")
+    adjective = request.args.get("adjective", "_ _ _'")
+    plural_noun = request.args.get("plural_noun", "_ _ _")
+
+    # Add all the story words into an attributes list
+    attributes = {"place": place, "noun": noun, "verb": verb,
+                  "adjective": adjective, "plural_noun": plural_noun}
+
+    # Get an instance of the story class with the story structure.
+    story_templates = {"default": default, "farmyard": farmyard, "pirate": pirate,
+                       "space": space, "superhero": superhero, "disaster": disaster}
+
+    blurb = story_templates[template].generate(attributes)
+
+    return render_template("story.html", story=blurb)
+
+
+default = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once upon a time in a long-ago {place}, there lived a
+       large {adjective} {noun}. It loved to {verb} {plural_noun}.""")
+
+farmyard = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Once in a hilarious {place}, there lived a wacky {adjective} {noun}.
+       It enjoyed {verb} with the crazy {plural_noun} all day long."""
+)
+
+pirate = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """In a hidden {place}, a brave {adjective} {noun} roamed, always
+       on a quest to {verb} for treasure with a bunch of goofy {plural_noun}."""
+)
+
+space = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Aboard a {place} in space, an {adjective} {noun} explored the cosmos.
+       Its mission: {verb} with the aliens and crack jokes with funny {plural_noun}."""
+)
+
+superhero = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """Above the city of {place}, a zany {adjective} {noun} fought crime
+       by {verb} and saving kittens stuck in trees. The cheers of the {plural_noun}
+       echoed all around."""
+)
+
+disaster = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+    """In a wild {place}, a time-traveling {adjective} {noun} went on epic adventures.
+       One day, it accidentally {verb} back to the age of the dinosaurs and met some
+       hilarious {plural_noun}."""
+)
